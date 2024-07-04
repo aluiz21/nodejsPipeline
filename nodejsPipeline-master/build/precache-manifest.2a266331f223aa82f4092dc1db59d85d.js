08b1a0eb08ab1f05733fed459b4fa4697b67b703
Lucas Gabriel <94190622+LucasGaab@users.noreply.github.com>
2024-07-04 16:10:04 -0300

    Add files via upload

---
+++ nodejsPipeline-master/build/precache-manifest.2a266331f223aa82f4092dc1db59d85d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63c8c3c6119914365794bd096afc0067",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19e7590f848f2fefc2e4",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9f6fdf7402e73b120f00",
+    "url": "/static/js/2.67f2198d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.67f2198d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19e7590f848f2fefc2e4",
+    "url": "/static/js/main.cf5531df.chunk.js"
+  },
+  {
+    "revision": "67c46fa5191a7506d6bc",
+    "url": "/static/js/runtime-main.bbba985a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
